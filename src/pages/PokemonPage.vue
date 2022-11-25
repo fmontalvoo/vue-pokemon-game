0a45372688239717c074f0c1592bcfcf2c2a8a96
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-11-25 18:03:34 -0500

    Se configura la estructura inicial.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,21 @@
+<template>
+    <h2>¿Qui&eacute;n es este pok&eacute;mon?</h2>
+    <PokemonPicture />
+    <PokemonOptions />
+</template>
+
+<script>
+import PokemonPicture from '@/components/PokemonPicture.vue'
+import PokemonOptions from '@/components/PokemonOptions.vue'
+
+export default {
+    components: {
+        PokemonPicture,
+        PokemonOptions,
+    }
+}
+</script>
+
+<style>
+
+</style>
